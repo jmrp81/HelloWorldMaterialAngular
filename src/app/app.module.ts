e6b3bb681cc348f5ca89ad14cefcb6fb297c1e98
Juan Manuel Rodriguez Perez <jmrp81@gmail.com>
2018-06-10 17:05:05 +0200

    Actualizado código

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
-// material
 import { CustomMaterial } from './Material/custom-material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-// components
 import { AppComponent } from './app.component';
-
-// services
 
 @NgModule({
   declarations: [
